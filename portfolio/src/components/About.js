cbaf09fa02c9c7569960eeb00598e507a81e7bb9
Stephanie Fajardo <stfajardo@gmail.com>
2022-03-29 18:32:25 -0400

    add the word finding

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -10,7 +10,7 @@ function About() {
           <img className='headshot mx-auto' alt='headshot of Steph Fajardo' src='/images/headshot.jpg'/>
           <div>
             <h1>Hi, I'm Steph</h1>
-            <p><i>I'm passionate about solving problems through efficient code, critical thinking, and creative solutions.</i></p>
+            <p><i>I'm passionate about solving problems through efficient code, critical thinking, and finding creative solutions.</i></p>
             <p>I recently graduated from Georgia Institute of Technology's coding bootcamp for full stack web development. Prior to this, I completed the Google UX design certificate course where I learned how to conduct UX research and design apps and websites that center user needs. I especially love working on projects that make an impact on social issues such as inequality and accessibility in design. I believe that we can use technology to make everyone's lives a little bit easier.</p>
             <p>Email me at<a href="mailto: stfajardo@gmail.com"> stfajardo@gmail.com</a></p> 
             <div>
