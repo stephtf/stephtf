b62e16f7b106bf0e3ab9fc0d379a0a906ad0ffad
Stephanie Fajardo <stfajardo@gmail.com>
2022-03-29 23:36:10 -0400

    add contact links

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,6 +1,7 @@
 import Header from './Header';
 import { NavLink } from 'react-router-dom';
 import Footer from './Footer';
+import ContactLinks from './header-components/ContactLinks';
 
 function About() {
     return (
@@ -14,12 +15,13 @@ function About() {
             <p>I recently graduated from Georgia Institute of Technology's coding bootcamp for full stack web development. Prior to this, I completed the Google UX design certificate course where I learned how to conduct UX research and design apps and websites that center user needs. I especially love working on projects that make an impact on social issues such as inequality and accessibility in design. I believe that we can use technology to make everyone's lives a little bit easier.</p>
             <p>Email me at<a href="mailto: stfajardo@gmail.com"> stfajardo@gmail.com</a></p> 
             <div>
-            <a href="./assets/resume.pdf" target="_blank"><button className='btn' id='resume-btn'>Resume</button></a>
-            <NavLink to='/my-story'><button className='btn' id='story-btn'>My Story</button></NavLink>
+              <a href="./assets/resume.pdf" target="_blank"><button className='btn' id='resume-btn'>Resume</button></a>
+              <NavLink to='/my-story'><button className='btn' id='story-btn'>My Story</button></NavLink>
+              <ContactLinks />
             </div>
           </div>
         </div>
-        <Footer />
+      <Footer />
       </div>
     );
   }
