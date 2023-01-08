f0fc2bf7140d1a6d6a06ec84539b102832ca7829
Stephanie Fajardo <stephaniefajardo@stephanies-mbp.lan>
2023-01-07 22:24:32 -0500

    add new email and resume

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
           <p><i>I'm passionate about solving problems through efficient code, critical thinking, and finding creative solutions.</i></p>
           <p>I recently graduated from Georgia Institute of Technology's coding bootcamp for full stack web development. Prior to this, I completed the Google UX design certificate course where I learned how to conduct UX research and design apps and websites that center user needs.
           </p>
-          <p>Email me at<a className='blue-link' href="mailto: stfajardo@gmail.com"> stfajardo@gmail.com</a></p>
+          <p>Email me at<a className='blue-link' href="mailto: stephfajardogarcia@gmail.com"> stephfajardogarcia@gmail.com</a></p>
           <div className='button-links'>
             <ContactLinks />
           </div>
